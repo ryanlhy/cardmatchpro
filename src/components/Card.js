172d2da4dde03fe2950d730a7b1e30e52ca1c64b
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-18 23:19:07 +0800

    added no results indicator, placeholder

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,37 +3,57 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Box, CardActionArea, Container, containerClasses } from '@mui/material';
+import { Box, CardActionArea, Container, containerClasses, Skeleton, Stack } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Avatar, Grid } from '@material-ui/core';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Grow from '@mui/material/Grow';
+import { Diversity1Sharp } from '@mui/icons-material';
+import BackgroundOverlay from './BackgroundOverlay';
+
 
 export default function ActionAreaCard(props) {
   const [pokemonArray, setPokemonArray] = useState([]);
   const selectedInput = useSelector((state) => state.search.searchSelectedValue);
   const matchFilterStrict = useSelector((state) => state.search.matchFilterStrict);
+  const selectedObj = useSelector((state) => state.search.searchSelectedObj);
   const [indexFilter, setIndexFilter] = useState([]);
+  const [loading, setLoading ] = useState(false);
+  const [growCards, setGrowCards] = useState(false);
   const dispatch = useDispatch();
-  console.log("selected input" + selectedInput)
-  const input = "charizard 11/108 holo psa 9"
+
   const callApiSearch = async () => {
     if (selectedInput === "") {
       console.log("did not call api from search");
     } else {
+      setLoading(true);
+
       // reset at every search query
-      const urlSrc = `https://ryanlhy.pythonanywhere.com/ebay/${selectedInput}`;
-      // const urlSrc = `http://localhost:8000/ebay/${selectedInput}`;
+      let paramsObj = {
+        search: selectedInput,
+        searchObj: selectedObj,
+      }
+      console.log(paramsObj)
+      let param = JSON.stringify(paramsObj)
+      // const urlSrc = `https://ryanlhy.pythonanywhere.com/ebay/${selectedInput}`;
+      const urlSrc = `http://localhost:8000/ebay/${param}`;
+      // const urlSrc = `http://localhost:8000/testparam/${param}`;
+
       apiFunc(urlSrc);
-      console.log(urlSrc)
     }
   };
   // console.log(pokemonArray)
 
   const apiFunc = async (urlSrc) => {
     const tempArray = pokemonArray; // temp array to reset if error
+    setGrowCards(true);
     try {
       const response = await fetch(urlSrc);
       const data = await response.json();
-      if (data.length !== 0) {
+      if (data.length !== 0 || data.data !== null) {
+        console.log("data: ", data);
         setPokemonArray(data.data);
         setIndexFilter(data.negative_keywords_index);
       }
@@ -41,6 +61,7 @@ export default function ActionAreaCard(props) {
       console.log("Error: ", err);
       setPokemonArray(tempArray);
     }
+    setLoading(false);
   };
   useEffect(() => {
     callApiSearch();
@@ -51,7 +72,8 @@ export default function ActionAreaCard(props) {
     return newImage;
   };
 
-  const ebayCards = pokemonArray.map((card, index) => {
+  const ebayCards = pokemonArray === null? <Stack>No results</Stack> : 
+  pokemonArray.map((card, index) => {
     // remove all cards with index in indexFilter, try to make it a filter
     if (matchFilterStrict === "Exact") {
       if (indexFilter.includes(index)) {
@@ -63,13 +85,22 @@ export default function ActionAreaCard(props) {
     // }
     return (
       <Card sx={{ maxWidth: 200, px:1, borderRadius:1}} key={index}>
-        <CardActionArea>
+        <Grow in={growCards} style={{ transformOrigin: '50% 100%'}} 
+        {...(growCards ? {timeout:2000} : {})}>
+
+        <CardActionArea sx={{}}>
+        
+          {/* position icon to top right and overlay image*/}
           <CardMedia
             component="img"
             height="200"
             image={changeImageAttribute(card.galleryURL[0])}
             alt="green iguana"
+            sx={{objectFit:'contain', borderRadius:1, border:1, borderColor:'black', borderStyle:'solid', position:'relative'}}
             />
+          {/* <Avatar sx={{position:'absolute', top:0, right:0, fill:'white', width:30, height:30,}}>
+            <FavoriteBorderIcon sx={{}}/>
+          </Avatar> */}
           <CardContent>
             <Typography gutterBottom variant="h7" component="div">
               {card.title}
@@ -84,17 +115,32 @@ export default function ActionAreaCard(props) {
               Time Left: {card.sellingStatus[0].timeLeft}
             </Typography>
           </CardContent>
+          {/* <AddShoppingCartIcon sx={{position:'absolute', bottom:0, right:0, color:'black'}}/> */}
         </CardActionArea>
+          </Grow>
       </Card>
     );
   });
 
   return (
     <Container>
-      <Box sx={{display:'flex',flexWrap: 'wrap', paddingX:1, paddingY:5, justifyContent: 'center'}}>
-        {ebayCards}
-
-      </Box>
+      <Grid container spacing={5} sx={{mx:'auto'}} >
+      {/* <BackgroundOverlay/> */}
+      {/* skeleton loading animation */}
+      {loading ? 
+        <Box sx={{  justifyContent: 'center', width: '100%', display:'flex',flexWrap: 'wrap'}}>
+          <Skeleton/>
+          <Skeleton animation="wave" />
+          <Skeleton animation={false} />
+        </Box> : 
+        <Box sx={{display:'flex',flexWrap: 'wrap', paddingX:1, paddingY:5, justifyContent: 'center'}}>
+          {ebayCards}
+        </Box> }
+          
+      {/* <Box sx={{display:'flex',flexWrap: 'wrap', paddingX:1, paddingY:5, justifyContent: 'center'}}>
+          {ebayCards}
+      </Box> */}
+      </Grid>
     </Container>
   );
 }
