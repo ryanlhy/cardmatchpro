ac0e508969b307933c1e0f7f4079817086552d07
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-24 16:46:09 +0800

    added modal logic to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Box, CardActionArea, Container, containerClasses, Skeleton, Stack } from '@mui/material';
+import { Box, CardActionArea, Container, containerClasses, Skeleton, Stack, Modal } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Avatar, Grid } from '@material-ui/core';
@@ -13,6 +13,7 @@ import Grow from '@mui/material/Grow';
 import { Diversity1Sharp } from '@mui/icons-material';
 import BackgroundOverlay from './BackgroundOverlay';
 import { buttonGradeTags, displayFilteredCards, apiResponseData, isButtonFilterOn } from '../store/searchSlice';
+import { Link } from 'react-router-dom';
 
 
 export default function ActionAreaCard(props) {
@@ -25,6 +26,8 @@ export default function ActionAreaCard(props) {
   const [indexFilter, setIndexFilter] = useState([]);
   const [loading, setLoading ] = useState(false);
   const [growCards, setGrowCards] = useState(false);
+  const [open, setOpen] = useState(false);
+
   const dispatch = useDispatch();
 
   const callApiSearch = async () => {
@@ -77,8 +80,8 @@ export default function ActionAreaCard(props) {
     callApiSearch();
   }, [selectedInput]);
 
-  const changeImageAttribute = (image) => {
-    const newImage = image.replace("s-l140", "s-l250");
+  const changeImageAttribute = (image, size) => {
+    const newImage = image.replace("s-l140", `s-l${size}`);
     return newImage;
   };
   console.log(displayFilteredCard)
@@ -92,20 +95,29 @@ export default function ActionAreaCard(props) {
         return null;
       }
     }
+
+    const handleOpen = () => {
+      setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+};
+  
+
     return (
         <Grow in={growCards} style={{ transformOrigin: '50% 100%'} } key={index}
         {...(growCards ? {timeout:2000} : {})}>
       <Card sx={{ maxWidth: 200, px:0, borderRadius:1, margin:1}} key={index}>
-
         <CardActionArea sx={{}}>
         
           {/* position icon to top right and overlay image*/}
           <CardMedia
             component="img"
             height="200"
-            image={changeImageAttribute(card.galleryURL[0])}
+            image={changeImageAttribute(card.galleryURL[0], 250)}
             alt="green iguana"
             sx={{objectFit:'contain', borderRadius:1, border:1, borderColor:'black', borderStyle:'solid', position:'relative'}}
+            onClick={handleOpen}
             />
           {/* <Avatar sx={{position:'absolute', top:0, right:0, fill:'white', width:30, height:30,}}>
             <FavoriteBorderIcon sx={{}}/>
@@ -117,15 +129,35 @@ export default function ActionAreaCard(props) {
             <Typography variant="body2" color="text.secondary">
               {card.sellingStatus[0].currentPrice[0]['@currencyId']}: {card.sellingStatus[0].currentPrice[0].__value__}
             </Typography>
+            
             <Typography variant="body2" color="text.secondary">
-              Bids: {card.sellingStatus[0].bidCount}
+              {`${card.sellingStatus[0].bidCount ? 
+                `Bids: ${card.sellingStatus[0].bidCount}`: 'Fixed Price'}`}
             </Typography>
+            
             <Typography variant="body2" color="text.secondary">
               Time Left: {card.sellingStatus[0].timeLeft}
+            </Typography >
+            <Typography variant="body2" color="text.secondary" component="a" href={card.viewItemURL[0]}>
+              See on ebay
             </Typography>
+            {/* </Link> */}
           </CardContent>
           {/* <AddShoppingCartIcon sx={{position:'absolute', bottom:0, right:0, color:'black'}}/> */}
         </CardActionArea>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            // bgColor: 'rgba(0, 0, 0, 0.5)',
+
+          }}
+        >
+          <img src={changeImageAttribute(card.galleryURL[0], 1000)} alt="Zoomed in image" />
+        </Modal>
       </Card>
           </Grow>
     );
