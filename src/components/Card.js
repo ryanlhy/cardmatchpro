ca22dd09675570e07ba386201cf300b31c603722
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-24 18:07:21 +0800

    refactor cardebay component

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,6 +14,7 @@ import { Diversity1Sharp } from '@mui/icons-material';
 import BackgroundOverlay from './BackgroundOverlay';
 import { buttonGradeTags, displayFilteredCards, apiResponseData, isButtonFilterOn } from '../store/searchSlice';
 import { Link } from 'react-router-dom';
+import CardEbay from './CardEbay';
 
 
 export default function ActionAreaCard(props) {
@@ -86,6 +87,7 @@ export default function ActionAreaCard(props) {
   };
   console.log(displayFilteredCard)
   console.log(pokemonArray);
+
   const ebayCards = pokemonArray === null? <Stack>No results</Stack> : 
   pokemonArray.map((card, index) => {
     // remove cards that do not match strict filter, and display only cards that match exact filter
@@ -94,73 +96,74 @@ export default function ActionAreaCard(props) {
       if (!displayFilteredCard.includes(card.itemId[0])) {
         return null;
       }
+    } else {
+      // return the ebay card
     }
-
-    const handleOpen = () => {
-      setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-};
+    return <CardEbay key={index} card={card}/>
+//     const handleOpen = () => {
+//       setOpen(true);
+//   };
+//   const handleClose = () => {
+//     setOpen(false);
+// };
   
-
-    return (
-        <Grow in={growCards} style={{ transformOrigin: '50% 100%'} } key={index}
-        {...(growCards ? {timeout:2000} : {})}>
-      <Card sx={{ maxWidth: 200, px:0, borderRadius:1, margin:1}} key={index}>
-        <CardActionArea sx={{}}>
+    // return (
+    //     <Grow in={growCards} style={{ transformOrigin: '50% 100%'} } key={index}
+    //     {...(growCards ? {timeout:2000} : {})}>
+    //   <Card sx={{ maxWidth: 200, px:0, borderRadius:1, margin:1}} key={index}>
+    //     <CardActionArea sx={{}}>
         
-          {/* position icon to top right and overlay image*/}
-          <CardMedia
-            component="img"
-            height="200"
-            image={changeImageAttribute(card.galleryURL[0], 250)}
-            alt="green iguana"
-            sx={{objectFit:'contain', borderRadius:1, border:1, borderColor:'black', borderStyle:'solid', position:'relative'}}
-            onClick={handleOpen}
-            />
-          {/* <Avatar sx={{position:'absolute', top:0, right:0, fill:'white', width:30, height:30,}}>
-            <FavoriteBorderIcon sx={{}}/>
-          </Avatar> */}
-          <CardContent>
-            <Typography gutterBottom variant="h7" component="div">
-              {card.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {card.sellingStatus[0].currentPrice[0]['@currencyId']}: {card.sellingStatus[0].currentPrice[0].__value__}
-            </Typography>
+    //       {/* position icon to top right and overlay image*/}
+    //       <CardMedia
+    //         component="img"
+    //         height="200"
+    //         image={changeImageAttribute(card.galleryURL[0], 250)}
+    //         alt="green iguana"
+    //         sx={{objectFit:'contain', borderRadius:1, border:1, borderColor:'black', borderStyle:'solid', position:'relative'}}
+    //         onClick={handleOpen}
+    //         />
+    //       {/* <Avatar sx={{position:'absolute', top:0, right:0, fill:'white', width:30, height:30,}}>
+    //         <FavoriteBorderIcon sx={{}}/>
+    //       </Avatar> */}
+    //       <CardContent>
+    //         <Typography gutterBottom variant="h7" component="div">
+    //           {card.title}
+    //         </Typography>
+    //         <Typography variant="body2" color="text.secondary">
+    //           {card.sellingStatus[0].currentPrice[0]['@currencyId']}: {card.sellingStatus[0].currentPrice[0].__value__}
+    //         </Typography>
             
-            <Typography variant="body2" color="text.secondary">
-              {`${card.sellingStatus[0].bidCount ? 
-                `Bids: ${card.sellingStatus[0].bidCount}`: 'Fixed Price'}`}
-            </Typography>
+    //         <Typography variant="body2" color="text.secondary">
+    //           {`${card.sellingStatus[0].bidCount ? 
+    //             `Bids: ${card.sellingStatus[0].bidCount}`: 'Fixed Price'}`}
+    //         </Typography>
             
-            <Typography variant="body2" color="text.secondary">
-              Time Left: {card.sellingStatus[0].timeLeft}
-            </Typography >
-            <Typography variant="body2" color="text.secondary" component="a" href={card.viewItemURL[0]}>
-              See on ebay
-            </Typography>
-            {/* </Link> */}
-          </CardContent>
-          {/* <AddShoppingCartIcon sx={{position:'absolute', bottom:0, right:0, color:'black'}}/> */}
-        </CardActionArea>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            // bgColor: 'rgba(0, 0, 0, 0.5)',
+    //         <Typography variant="body2" color="text.secondary">
+    //           Time Left: {card.sellingStatus[0].timeLeft}
+    //         </Typography >
+    //         <Typography variant="body2" color="text.secondary" component="a" href={card.viewItemURL[0]}>
+    //           See on ebay
+    //         </Typography>
+    //         {/* </Link> */}
+    //       </CardContent>
+    //       {/* <AddShoppingCartIcon sx={{position:'absolute', bottom:0, right:0, color:'black'}}/> */}
+    //     </CardActionArea>
+    //     <Modal
+    //       open={open}
+    //       onClose={handleClose}
+    //       style={{
+    //         display: 'flex',
+    //         alignItems: 'center',
+    //         justifyContent: 'center',
+    //         // bgColor: 'rgba(0, 0, 0, 0.5)',
 
-          }}
-        >
-          <img src={changeImageAttribute(card.galleryURL[0], 1000)} alt="Zoomed in image" />
-        </Modal>
-      </Card>
-          </Grow>
-    );
+    //       }}
+    //     >
+    //       <img src={changeImageAttribute(card.galleryURL[0], 1000)} alt="Zoomed in image" />
+    //     </Modal>
+    //   </Card>
+    //       </Grow>
+    // );
 
   });
 
