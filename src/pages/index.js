3a5c485d0ae091f53a217c661b3b647f754bc560
ryanlaw <ryan_law-rebound@hotmail.com>
2023-04-15 13:18:47 +0800

    added pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,25 +5,23 @@ import FreeSolo from "../components/SearchBar";
 import ActionAreaCard from "../components/Card";
 import DataGridDemo from "../components/filter";
 import DescreteSlider from "../components/FilterSlider";
-import { ThemeProvider } from './../util/theme';
+import { ThemeProvider } from "./../util/theme";
 import BackgroundImage from "../components/BackgroundImage";
 import BackgroundOverlay from "../components/BackgroundOverlay";
 import ButtonTags from "../components/ButtonTags";
 import SearchTagSection from "../components/SearchTagSection";
 import Logo from "../components/Logo";
 
-
-
 function IndexPage(props) {
-  console.log(props)
+  console.log(props);
   return (
     <>
       <Meta />
       <ThemeProvider>
-      {/* <BackgroundOverlay/> */}
-      <Logo/>
-      <FreeSolo maxWidth="md"  />
-      {/* <SearchTagSection/> */}
+        {/* <BackgroundOverlay/> */}
+        <Logo />
+        <FreeSolo maxWidth="md" />
+        {/* <SearchTagSection/> */}
       </ThemeProvider>
       {/* <DescreteSlider/> */}
       {/* <DataGridDemo/> */}
