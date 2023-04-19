700bcbb1e19a4cdf0c1a4e609d6bbcade333aa43
ryanlaw <ryan_law-rebound@hotmail.com>
2023-04-19 16:21:49 +0800

    refactor code to accept individual changes

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -13,20 +13,22 @@ export const userSlice = createSlice({
     is_active: null,
   },
   reducers: {
-    saveDetails: (state, action) => {
-      state.id = action.payload.id;
-      state.name = action.payload.name;
-      state.email = action.payload.email;
-      state.address = action.payload.address;
-      state.uid = action.payload.uid;
-      state.signup_date = action.payload.signup_date;
-      state.signin_date = action.payload.signin_date;
-      state.is_active = action.payload.is_active;
+    saveName: (state, action) => {
+      state.name = action.payload;
+    },
+    saveEmail: (state, action) => {
+      state.email = action.payload;
+    },
+    saveAddress: (state, action) => {
+      state.address = action.payload;
+    },
+    saveUId: (state, action) => {
+      state.uid = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { saveDetails } = userSlice.actions;
+export const { saveName, saveEmail, saveAddress, saveUId } = userSlice.actions;
 
 export default userSlice.reducer;
